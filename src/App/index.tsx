6b9252af619b0347b2da74a9b1811f32d743a074
ix-s9y <154841633+ix-s9y@users.noreply.github.com>
2023-12-26 09:36:27 +0900

    [Fix] title in html and <App>

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,7 +5,7 @@ import { circle } from '~/sketch';
 export const App = () => {
   return (
     <>
-      <h1>React + TypeScript + p5js Template</h1>
+      <h1>Simple Ray Tracing with React + TypeScript + p5js</h1>
       <Canvas sketch={circle} />
     </>
   );
